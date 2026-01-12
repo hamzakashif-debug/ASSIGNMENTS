addf0866a1c86b97d8293977858f46f054d1bbcd
hamzakashif-debug <hamza.kashif5006@gmail.com>
2026-01-12 17:10:55 +0500

    Add files via upload

---
+++ POWER.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class POWER {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER VOLTMETER READING : ");
+        double V = input.nextDouble();
+        System.out.print("ENTER AMMETER READING : ");
+        double I = input.nextDouble();
+        double P = I*V;
+        System.out.print("Your Calculated Power Is "+ P + "W"); 
+    
+    }
+}
