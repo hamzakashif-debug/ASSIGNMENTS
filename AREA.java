62099eddb7b80b6171973192bb6c1d77e757a906
hamzakashif-debug <hamza.kashif5006@gmail.com>
2026-01-12 17:29:50 +0500

    Add files via upload

---
+++ AREA.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AREA {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter Hieght Of Trapezoid : ");
+        double h= input.nextDouble();
+        System.out.print("Enter Two Parallel Sides : " );
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double K = (a+b)/2*h;
+        System.out.print("Area Of Trapezoid Is : "+K);
+    }
+    
+}
